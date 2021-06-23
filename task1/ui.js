072dbfaddd053450ea7279fa0da855b4358a0a50
TheBakari <draganic69@gmail.com>
2021-06-23 09:37:44 +0200

    morph task 1

---
+++ task1/ui.js
@@ -0,0 +1,16 @@
+var numb = document.querySelector(".number");
+var counter = 0;
+setInterval(() => {
+  if(counter == 25 ){
+    clearInterval();
+  }
+  else{// else if (counter-=eval.count)
+    counter+=1;
+    numb.textContent = counter + "%";
+  }
+}, 80);
+
+function focus(){
+  alert("input filed is empry")
+}
+
